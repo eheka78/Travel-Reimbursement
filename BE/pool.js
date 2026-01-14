5142668c7af7b302dd3b7bc79e6c282d897928ed
eheka78 <gemddkim22@gmail.com>
2026-01-15 06:04:06 +0900

    .

--- BE/pool.js
+++ BE/pool.js
@@ -3,15 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
+const pool = await mysql.createConnection({
 	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	port: 3000,
-	waitForConnections: true,
-	connectionLimit: 10,
-	queueLimit: 0,
+	user: process.env.DB_USER, 
+	password: process.env.DB_PASSWORD, 
+	database: process.env.DB_NAME, 
+	port: 3306
 });
 
 console.log("✅ MySQL Pool 생성 완료");
